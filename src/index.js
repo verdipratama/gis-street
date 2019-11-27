087a478a2c763e24820a820b275338b1bd433939
verdipratama <verdipratama@icloud.com>
2019-11-27 10:22:36 +0700

    clean up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+// styles
+import '../src/assets/css/bootstrap.min.css';
+import '../src/assets/css/now-ui-kit.css';
+
+import { Jumbotron, Button } from 'reactstrap';
+
 import * as serviceWorker from './serviceWorker';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App-wrapper">
+        <div className=" mx-auto text-center">
+          <Jumbotron>
+            <h1 className="display-3">Hello, world!</h1>
+            <p className="lead">
+              This is a simple hero unit, a simple Jumbotron-style component for calling extra
+              attention to featured content or information.
+            </p>
+            <hr className="my-2" />
+            <p>
+              It uses utility classes for typography and spacing to space content out within the
+              larger container.
+            </p>
+            <p className="lead">
+              <Button color="primary">Learn More</Button>
+            </p>
+          </Jumbotron>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
